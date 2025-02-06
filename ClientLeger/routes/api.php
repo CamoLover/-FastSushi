029b440d996b8d8085d22f59e1a3af1c3325944e
Jayden <146840699+Starman404@users.noreply.github.com>
2025-02-06 08:52:39 +0100

    1.5 | Authentification utilisateur - Vérification des identifiants (Back - POST)

--- ClientLeger/routes/api.php
+++ ClientLeger/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PasswordResetController;
 use App\Http\Controllers\CreaCompteController;
+use App\Http\Controllers\SigninController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,3 +14,4 @@ Route::post('/reset-password', [PasswordResetController::class, 'sendResetLink']
 
 Route::post('/create-accompte', [CreaCompteController::class, 'createAccount']);
 
+Route::post('/signin-bdd', [SigninController::class, 'signin']);
