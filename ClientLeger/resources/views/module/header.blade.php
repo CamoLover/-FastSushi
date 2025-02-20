ccfdd2042b7711d72b1f87329a2022fe6c7f9a6b
Evan E <evan.escabasse@gmail.com>
2025-02-20 13:08:35 +0100

    changed Title color

--- ClientLeger/resources/views/module/header.blade.php
+++ ClientLeger/resources/views/module/header.blade.php
@@ -1,7 +1,7 @@
 <header id="floatingHeader" class="fixed top-4 left-1/2 transform -translate-x-1/2 w-[90%] bg-[#252422] py-4 px-8 flex justify-between items-center shadow-lg border-b border-[#D90429] rounded-xl transition-transform duration-300">
     <!-- Logo -->
-    <div class="text-2xl font-bold tracking-wide transition-transform transform hover:scale-110 text-[#CCC5B9]">
-        <a href="/" class="hover:text-[#FFFCF2] transition duration-300">Fast Sushi</a>
+    <div class="text-2xl font-bold tracking-wide transition-transform transform hover:scale-110 text-[#ce0006]">
+        <a href="/" class="hover:text-[#ce0006] transition duration-300">Fast Sushi</a>
     </div>
 
     <!-- Navigation -->
